cd0e157e95d2a4de57030422206633440b4c6ff9
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2023-12-31 00:10:08 +0900

    codegenをセットアップ

---
+++ codegen.ts
@@ -0,0 +1,17 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "schema.graphql",
+  generates: {
+    "src/types/generated/graphql.ts": {
+      plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        contextType: "../context#Context"
+      }
+    }
+  }
+};
+
+export default config;
